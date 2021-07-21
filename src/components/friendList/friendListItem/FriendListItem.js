756827fec07b054e4c5289a462650df5d9da771c
Kuliiev Roman <Mejas.black@gmail.com>
2021-07-21 16:29:37 +0300

    Final

---
+++ src/components/friendList/friendListItem/FriendListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  FriendListItemContainer,
+  FriendListItemStatus,
+} from "./FriendListItemStyled";
+
+export const FriendListItem = ({ avatar, name, isOnline }) => (
+  <FriendListItemContainer>
+    <FriendListItemStatus isOnline={isOnline} />
+    <img className="avatar" src={avatar} alt="" width="48" />
+    <p className="name">{name}</p>
+  </FriendListItemContainer>
+);
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
