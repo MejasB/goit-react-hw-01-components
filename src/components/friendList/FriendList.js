1f3f02de937b792b17d72290be01aa991f0495ca
Kuliiev Roman <Mejas.black@gmail.com>
2021-09-23 17:01:15 +0300

    finished

--- src/components/friendList/FriendList.js
+++ src/components/friendList/FriendList.js
@@ -2,18 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import { FriendListItem } from "./friendListItem/FriendListItem";
 
-const FriendList = ({ friends }) => (
-  <ul className="friend-list">
-    {friends.map((friend) => (
-      <FriendListItem
-        avatar={friend.avatar}
-        name={friend.name}
-        isOnline={friend.isOnline}
-        key={friend.id}
-      />
-    ))}
-  </ul>
-);
+const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+          key={id}
+        />
+      ))}
+    </ul>
+  );
+};
 
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(PropTypes.object),
